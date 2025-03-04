5ce58894d4b157e5bf19909655bb457c910fead5
sasha <sasha.burmey@gmail.com>
2025-03-04 15:34:13 +0200

    Set application locale in PageRoutesService
    Ensure the application locale is correctly set before generating the URI. This aligns the locale context for subsequent operations and prevents potential inconsistencies.

--- src/Services/PageRoutesService.php
+++ src/Services/PageRoutesService.php
@@ -111,6 +111,7 @@ class PageRoutesService
         if (!in_array($locale, FilamentFabricator::getLocales())) {
             $locale = FilamentFabricator::getDefaultLocale();
         }
+        app()->setLocale($locale);
         $uri = Str::start($uri, "{$locale}/");
         $id = $this->getPageIdFromUri($uri);
 
